1eacfe7cbb62509dad1c1318552cb69072ad98bc
Omega <omega@alpha.org>
2015-01-19 10:31:48 -0500

    Fifth commit

--- rust.rs
+++ rust.rs
@@ -2,7 +2,7 @@ use std::io::TcpStream;
 use std::io::BufferedStream;
 fn main() {
 	let nick = "RustBOT";
-	let mut channel = "#mootsinsuits";
+	let channel = "#mootsinsuits";
 	let nick_packet = format!("NICK {}\r\n", nick);
 	let user_packet = format!("USER {} {} {} :{}\r\n", nick, nick, nick, nick);
 	let join_packet = format!("JOIN {}\r\n", channel);
@@ -16,10 +16,10 @@ fn main() {
 	while(y == 1) {
 		let buffer = socket.read_line().unwrap();
 		println!("{}", buffer.trim());
+		let mut chan = "";
 		if buffer.contains("PRIVMSG ") {
 			let mut data = buffer.split_str("PRIVMSG ").nth(1).unwrap();
-			let chan = data.split_str(" :").nth(0).unwrap().trim();
-			channel = chan;
+			chan = data.split_str(" :").nth(0).unwrap().trim();
 		}
 		if buffer.contains("396") {
 			socket.write(join_packet.as_bytes());
@@ -37,6 +37,7 @@ fn main() {
 			let mut chan = buffer.split_str(".j ").nth(1).unwrap();
 			let j = format!("JOIN {}\r\n", chan);
 			socket.write(j.as_bytes());
+			socket.flush();
 			println!("Sent join channel");
 		}
 		if buffer.contains(".p ") {
@@ -52,7 +53,7 @@ fn main() {
 			y = 0;
 		}
 		if buffer.contains(".rust") {
-			let r = format!("PRIVMSG {} :I am a bot made in Rust.\r\n", channel);
+			let r = format!("PRIVMSG {} :I am a bot made in Rust.\r\n", chan);
 			socket.write(r.as_bytes());
 			socket.flush();
 		}
